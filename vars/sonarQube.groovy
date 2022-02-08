4a637e96e963a5980010eb2007ce2b0c18128345
Mohit722 <91499522+Mohit722@users.noreply.github.com>
2022-02-09 02:57:13 +0530

    Create sonarQube.groovy

---
+++ vars/sonarQube.groovy
@@ -0,0 +1,9 @@
+def test(def projectkey){
+    def scannerHome = tool 'sonarqube'
+    withSonarQubeEnv('sonarqube'){
+    sh "${scannerHome}/bin/sonar-scanner \
+    -Dsonar.projectKey=${projectkey} \
+      -Dsonar.exclusions=vendor/**,resources/**,**/*.java,.scannerwork/** " 
+ }
+ sh "sudo rm -rf ${WORKSPACE}/.scannerwork"
+}
